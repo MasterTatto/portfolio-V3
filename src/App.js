737212aaaee24bee43dd25178de60b2841c011f9
Alex <gromvolk97@gmail.com>
2021-12-07 07:05:50 +0300

    added form contact

--- src/App.js
+++ src/App.js
@@ -1,52 +1,45 @@
 import './App.scss';
-import Header from "./feature/header";
-import Home from "./feature/home";
-import Portfolio from "./feature/portfolio";
-import {Route, Routes} from "react-router-dom";
-import Resume from "./feature/resume";
-import Skills from "./feature/skills";
-import Contact from "./feature/contact";
-import {useState} from "react";
-import {useWindowSize} from "./helpers/windowsSize";
-import {combineCss} from "./helpers/combineCss";
-import Bubble from "./helpers/bubble";
-import Cursor from "react-special-cursor";
-import AnimatedCursor from "react-animated-cursor";
+import Header from './feature/header';
+import Home from './feature/home';
+import Portfolio from './feature/portfolio';
+import { Route, Routes } from 'react-router-dom';
+import Resume from './feature/resume';
+import Skills from './feature/skills';
+import Contact from './feature/contact';
+import { useState } from 'react';
+import { useWindowSize } from './helpers/windowsSize';
+import { combineCss } from './helpers/combineCss';
+import Bubble from './helpers/bubble';
+import AnimatedCursor from 'react-animated-cursor';
 
 function App() {
-    const [currentUrl, setCurrentUrl] = useState('/')
-    const [showMenu, setShowMenu] = useState(false)
-    const {width} = useWindowSize()
-    console.log(window)
+	const [currentUrl, setCurrentUrl] = useState('/');
+	const [showMenu, setShowMenu] = useState(false);
+	const { width } = useWindowSize();
+	console.log(width);
 
-    return (
-        <>
-            <AnimatedCursor
-                innerSize={8}
-                outerSize={40}
-                color='8, 253, 216'
-                outerAlpha={.3}
-                innerScale={1}
-                outerScale={2.1}
-            />
-            <Header currentUrl={currentUrl} setCurrentUrl={setCurrentUrl} width={width} showMenu={showMenu}
-                    setShowMenu={setShowMenu}/>
+	return (
+		<>
+			<AnimatedCursor innerSize={8} outerSize={40} color='8, 253, 216' outerAlpha={0.3} innerScale={1} outerScale={2.1} />
+			<Header currentUrl={currentUrl} setCurrentUrl={setCurrentUrl} width={width} showMenu={showMenu} setShowMenu={setShowMenu} />
 
-            {width <= 768 && <span className={combineCss('showMenu', showMenu && 'hidden')}
-                                   onClick={() => setShowMenu(true)}>menu</span>}
-            <div className='pages'>
-                <Routes>
-                    <Route exact path={'*'} element={<Home currentUrl={currentUrl}/>}/>
-                    <Route path={'/portfolio'} element={<Portfolio currentUrl={currentUrl}/>}/>
-                    <Route path={'/resume'} element={<Resume currentUrl={currentUrl}/>}/>
-                    <Route path={'/skills'} element={<Skills currentUrl={currentUrl}/>}/>
-                    <Route path={'/contact'} element={<Contact currentUrl={currentUrl}/>}/>
-                </Routes>
-                {width > 550 && <Bubble size={25}/>}
-            </div>
-
-        </>
-    )
-};
+			{width <= 768 && (
+				<span className={combineCss('showMenu', showMenu && 'hidden')} onClick={() => setShowMenu(true)}>
+					menu
+				</span>
+			)}
+			<div className='pages'>
+				<Routes>
+					<Route exact path={'*'} element={<Home currentUrl={currentUrl} />} />
+					<Route path={'/portfolio'} element={<Portfolio currentUrl={currentUrl} />} />
+					<Route path={'/resume'} element={<Resume currentUrl={currentUrl} />} />
+					<Route path={'/skills'} element={<Skills currentUrl={currentUrl} />} />
+					<Route path={'/contact'} element={<Contact currentUrl={currentUrl} />} />
+				</Routes>
+				{width > 550 && <Bubble size={25} />}
+			</div>
+		</>
+	);
+}
 
 export default App;
--- src/feature/contact/index.js
+++ src/feature/contact/index.js
@@ -1,22 +1,64 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './styles.module.scss';
-import {combineCss} from "../../helpers/combineCss";
+import { combineCss } from '../../helpers/combineCss';
+import { ReactComponent as ContactMe } from '../../assets/icons/contactMe.svg';
+import Container from '../../helpers/container';
+import emailjs from 'emailjs-com';
+import { init } from 'emailjs-com';
 
-const Contact = ({currentUrl}) => {
-    let [left, setLeft] = useState(false);
-    useEffect(() => {
-        if (currentUrl === '/contact') {
-            setTimeout(() => {
-                setLeft(true)
-            }, 0)
-        }
-    })
-    return (
-        <div className={combineCss(styles.portfolio, left && styles.active)}>
-            <h1>Contact</h1>
-        </div>
+const Contact = ({ currentUrl }) => {
+	init('user_o9lBAZW5hXTb1BQ0r6faI');
 
-    )
+	let [left, setLeft] = useState(false);
+	useEffect(() => {
+		if (currentUrl === '/contact') {
+			setTimeout(() => {
+				setLeft(true);
+			}, 0);
+		}
+	});
+
+	const form = useRef();
+
+	const sendEmail = (e) => {
+		e.preventDefault();
+
+		emailjs.sendForm('service_elap3uh', 'template_2gya86a', form.current, 'user_o9lBAZW5hXTb1BQ0r6faI').then(
+			(result) => {
+				alert(result.text);
+			},
+			(error) => {
+				console.log(error.text);
+			}
+		);
+	};
+
+	return (
+		<div className={combineCss(styles.portfolio, left && styles.active)}>
+			<Container>
+				<div className={styles.wrapper}>
+					<div className={styles.imgBox}>
+						<ContactMe className={styles.img} />
+					</div>
+					<div className={styles.inputsBox}>
+						<form ref={form} className={styles.formStyles} onSubmit={sendEmail}>
+							<div className={styles.topInputs}>
+								<input name='name' className={combineCss(styles.input)} type='text' placeholder='Name' />
+								<input name='email' className={combineCss(styles.input)} type='text' placeholder='Email' />
+							</div>
+							<input className={combineCss(styles.input)} type='text' placeholder='Subject' />
+							<textarea name='messages' className={combineCss(styles.input, styles.inputArea)} placeholder='Messages'></textarea>
+							<div>
+								<button href='#' className={styles.btn} type='submit'>
+									Send messages!
+								</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</Container>
+		</div>
+	);
 };
 
 export default Contact;
